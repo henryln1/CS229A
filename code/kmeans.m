d540271fad99292c83d6015e5266441fa152d39d
Joseph Yen <joseph@DN0a1fe10c.SUNet>
2018-12-09 20:02:12 -0800

    more clusters

--- code/kmeans.m
+++ code/kmeans.m
@@ -3,35 +3,25 @@ close all
 X = csvread('cleaned_continuous_diabetic_data_normalized.csv',1,0);
 max_iters = 100;
 K = 3;
-assignments = zeros(size(X,1),10);
-for i = 1:10
-    initial_centroids = kMeansInitCentroids(X, K);
-    [centroids, idx] = runkMeans(X, initial_centroids, max_iters);
-    assignments(:,i) = idx;
-end
+assignments = zeros(size(X,1),3);
 
-filename = 'assignments.xlsx';
-xlswrite(filename,assignments,1)
-
-%{
 % Gender, age
-temp = [X(:,1) X(:,2)];
+temp = [X(:,1) X(:,2) X(:,end)];
 initial_centroids = kMeansInitCentroids(temp, K);
 [centroids, idx] = runkMeans(temp, initial_centroids, max_iters);
 assignments(:,1) = idx;
 
 % Time in hospital, num lab procedures, num procedures
-temp = [X(:,3) X(:,4) X(:,5)];
+temp = [X(:,3) X(:,4) X(:,5) X(:,end)];
 initial_centroids = kMeansInitCentroids(temp, K);
 [centroids, idx] = runkMeans(temp, initial_centroids, max_iters);
 assignments(:,2) = idx;
 
 % Num medications, diabetes medication
-temp = [X(:,6) X(:,12)];
+temp = [X(:,6) X(:,12) X(:,end)];
 initial_centroids = kMeansInitCentroids(temp, K);
 [centroids, idx] = runkMeans(temp, initial_centroids, max_iters);
 assignments(:,3) = idx;
 
 filename = 'assignments.xlsx';
 xlswrite(filename,assignments,1)
-%}
